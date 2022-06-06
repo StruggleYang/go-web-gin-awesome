bdeff187184702df3de21b2b46c4027f49f339bb
struy <1724555319@qq.com>
2022-06-06 17:46:48 +0800

    init

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"awesomeProject/controllers"
+	"awesomeProject/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	r.Run()
+}
