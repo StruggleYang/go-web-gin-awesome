5379ae8ce67683d117ecb36ca8270622ec98304f
struy <1724555319@qq.com>
2022-06-06 17:58:54 +0800

    web html

--- main.go
+++ main.go
@@ -12,8 +12,15 @@ func main() {
 
 	models.ConnectDatabase()
 
+	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
 	})
 	r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
